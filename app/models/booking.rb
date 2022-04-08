bb836eafb6ff6846afa594af97719b2eaa05258a
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-04-08 12:14:26 -0300

    action text installed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
   validates :arrival, :departure, presence: true
+  # validates :arrival, comparison: { less_than: :departure }
 end
