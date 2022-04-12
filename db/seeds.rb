6c265e568af53a47b99fa7712ddc1a590906bd77
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-04-12 15:48:04 -0300

    link_to with query for homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # require "faker"
 
 # CITY = ["Paris", "Marseille", "Lyon", "Nice", "Bordeaux", "Lens", "Montpellier",
-#         "Strasbourg", "Madrid", "Barcelone", "Seville", "Rome", "Berlin", "Munich",
-#         "Porto", "Milan", "Londres"]
-CITY = ["Paris", "Marseille", "Lyon", "Nice", "Bordeaux", "Lens", "Montpellier",
-        "Strasbourg", "Toulouse", "Lille", "Saint -Etienne", "Metz", "Toulon", "Caen", "Nantes"]
+#         "Strasbourg", "Toulouse", "Lille", "Saint -Etienne", "Metz", "Toulon",
+#         "Caen", "Nantes"]
+CITY = ["Paris", "Marseille", "Lyon", "Nice", "Bordeaux", "Montpellier",
+        "Toulouse", "Lille"]
 ADDRESS = ["#{(27..150).to_a.sample}, place Rémy Barbe", "#{(27..150).to_a.sample}, rue Charlotte Torres", "#{(27..150).to_a.sample}, impasse Bouvier", "#{(27..150).to_a.sample}, impasse Martineau","#{(27..150).to_a.sample}, avenue Vallee", "#{(27..150).to_a.sample}, boulevard de Poirier", "#{(27..150).to_a.sample}, rue de Brunel"]
 FLAT_GENRE = ["Studio", "Appartement", "Maisonnette", "Loft", "T1", "T2", "T3", "T4", "T5"]
 FLAT_NAME = ["Le Gîte du Cèdre Bleu","La Famille Réunie","Au Sommet du Monde","Gîte à la Campagne","Chambre Sérénité","Chambre Plein Océan","Le Gîte Oublié","Le Rivage Bleu","Villa Éole","Duo des Jardins","Le Domaine Coloré","Les Terrasses Ensoleillées","La Robe Rose","Le Patio Bleu","Les Châteaux Bleus","La Résidence Alizé","Espace Estival","Sensation Printemps","Villa Lumière"]
