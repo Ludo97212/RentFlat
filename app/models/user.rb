cc134ba24660958e00b749d3f5e276d27c3ab867
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-06-09 17:58:04 -0300

    deleted first and lastname for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, :first_name, presence: true
+  validates :username, uniqueness: true, presence: true
 
   has_many :flats, dependent: :destroy
   has_many :bookings, dependent: :destroy
