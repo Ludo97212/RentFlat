bf142df532ba8e459ea959670878b10bf0f59d12
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-07-25 19:14:57 -0300

    added validations for email format and password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_confirmation_of :password
+  validates_presence_of :password, on: :create
+  validates :password, length: { minimum: 8, maximum: 50 }
+  validates :email, uniqueness: true, presence: true
   validates :username, uniqueness: true, presence: true
-  validates :username, length: {in: 5..50}
+  validates :username, length: { in: 5..50 }
+
+  # ^(?=.*[A-Z])(?=.*[?!@#$&*,;:ù%.§µ£¤])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,20}$
+  # /\A([^\}\{\]\[@\s\,]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, format: { with: /\A([^\}\{\]\[@\s\,]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: "Mauvais format d'adresse email" }
 
   has_many :flats, dependent: :destroy
   has_many :bookings, dependent: :destroy
